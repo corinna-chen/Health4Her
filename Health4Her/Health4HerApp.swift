86307b871cecbb69de89f6c7bdb6aa8d750759a6
Corinna Chen <cchen9023@bths.edu>
2023-08-04 09:00:09 -0400

    Initial Commit

---
+++ Health4Her/Health4HerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health4HerApp.swift
+//  Health4Her
+//
+//  Created by Scholar on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Health4HerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
